68a80e3751e685caace4d85b669f84ad6cdfa9bf
Kirill.Suptelo <kirill.suptelov@gmail.com>
2022-06-20 19:49:15 +0700

    new file:   .gitignore 	new file:   .mvn/wrapper/maven-wrapper.jar 	new file:   .mvn/wrapper/maven-wrapper.properties 	new file:   mvnw 	new file:   mvnw.cmd 	new file:   pom.xml 	new file:   src/main/java/com/github/thecoolersuptelov/msgbackend/MsgbackendApplication.java 	new file:   src/main/resources/application.properties 	new file:   src/test/java/com/github/thecoolersuptelov/msgbackend/MsgbackendApplicationTests.java

---
+++ src/main/java/com/github/thecoolersuptelov/msgbackend/MsgbackendApplication.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.msgbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsgbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsgbackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/thecoolersuptelov/msgbackend/MsgbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.thecoolersuptelov.msgbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsgbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
